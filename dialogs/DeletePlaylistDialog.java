9ef27581d987170d3f0972e99ba0bee818851d6c
Oral Alikhan <oralalikhan@gmail.com>
2022-05-22 20:02:04 +0900

    Add files via upload

---
+++ dialogs/DeletePlaylistDialog.java
@@ -0,0 +1,64 @@
+package com.kabouzeid.gramophone.dialogs;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import android.text.Html;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.kabouzeid.gramophone.R;
+import com.kabouzeid.gramophone.model.Playlist;
+import com.kabouzeid.gramophone.util.PlaylistsUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+public class DeletePlaylistDialog extends DialogFragment {
+
+    @NonNull
+    public static DeletePlaylistDialog create(Playlist playlist) {
+        List<Playlist> list = new ArrayList<>();
+        list.add(playlist);
+        return create(list);
+    }
+
+    @NonNull
+    public static DeletePlaylistDialog create(List<Playlist> playlists) {
+        DeletePlaylistDialog dialog = new DeletePlaylistDialog();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList("playlists", new ArrayList<>(playlists));
+        dialog.setArguments(args);
+        return dialog;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //noinspection unchecked
+        final List<Playlist> playlists = getArguments().getParcelableArrayList("playlists");
+        int title;
+        CharSequence content;
+        //noinspection ConstantConditions
+        title = R.string.delete_playlists_title;
+        if (playlists.size() > 1) {
+            content = Html.fromHtml(getString(R.string.delete_x_playlists, playlists.size()));
+        } else {
+            content = Html.fromHtml(getString(R.string.delete_playlist_x, playlists.get(0).name));
+        }
+        return new MaterialDialog.Builder(getActivity())
+                .title(title)
+                .content(content)
+                .positiveText(R.string.delete_action)
+                .negativeText(android.R.string.cancel)
+                .onPositive((dialog, which) -> {
+                    if (getActivity() == null)
+                        return;
+                    PlaylistsUtil.deletePlaylists(getActivity(), playlists);
+                })
+                .build();
+    }
+}
